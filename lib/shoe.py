1e085bd0bbc933ba66b2e6dccae2a6925407fa8c
Johey Hargrett <joheyhargrett@gmail.com>
2023-11-28 08:50:47 -0500

    completed

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+
+    def __init__(self, product_brand, product_size):
+        self.brand = product_brand
+        self.size = product_size
+        self.condition = "New after repair"
+        
+        
+    def repair(self):
+        self.condition = "New"
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+        
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+            
+    
